15c2caad0f56f44ceb3763e8ef0ec9fb16f2e0a0
shanelykins <shanewlykins@gmail.com>
2025-11-05 12:20:41 -0800

    Updated index.tsx

--- src/sections/Hero/index.tsx
+++ src/sections/Hero/index.tsx
@@ -11,29 +11,6 @@ export const Hero = () => {
             <div className="absolute bg-[linear-gradient(rgba(92,192,209,0)_12.89%,rgb(128,111,100)_26.29%,rgb(128,111,100)_56.58%)] box-border caret-transparent block h-3/5 w-full z-[2] left-0 bottom-0 md:static md:bg-none md:hidden md:h-auto md:w-auto md:z-10 md:left-auto md:bottom-auto"></div>
             <HeroImage />
           </div>
-          <section className="[mask-image:radial-gradient(115.24px_at_50%_137.5px,rgb(0,0,0)_99%,rgba(0,0,0,0)_101%),radial-gradient(115.24px_at_50%_-112.5px,rgba(0,0,0,0)_99%,rgb(0,0,0)_101%),radial-gradient(115.24px_at_50%_calc(100%_-_137.5px),rgb(0,0,0)_99%,rgba(0,0,0,0)_101%),radial-gradient(115.24px_at_50%_calc(100%_+_112.5px),rgba(0,0,0,0)_99%,rgb(0,0,0)_101%)] relative items-center bg-violet-950 box-border caret-transparent flex flex-col justify-center mb-[-50px] mt-[-50px] [mask-clip:border-box,border-box,border-box,border-box] [mask-composite:add,add,add,add] [mask-mode:match-source,match-source,match-source,match-source] [mask-origin:border-box,border-box,border-box,border-box] [mask-repeat:repeat-x,repeat-x,repeat-x,repeat-x] [mask-size:100px_51%,100px_calc(51%_-_25px),100px_51%,100px_calc(51%_-_25px)]  z-[3] px-[4%] py-[50px] md:[mask-image:radial-gradient(182px_at_50%_225px,rgb(0,0,0)_99%,rgba(0,0,0,0)_101%),radial-gradient(182px_at_50%_-175px,rgba(0,0,0,0)_99%,rgb(0,0,0)_101%),radial-gradient(182px_at_50%_calc(100%_-_225px),rgb(0,0,0)_99%,rgba(0,0,0,0)_101%),radial-gradient(182px_at_50%_calc(100%_+_175px),rgba(0,0,0,0)_99%,rgb(0,0,0)_101%)] md:mb-[-75px] md:mt-[-75px] md:[mask-size:200px_51%,200px_calc(51%_-_50px),200px_51%,200px_calc(51%_-_50px)] md: md:py-[75px]">
-            <div className="items-center box-border caret-transparent gap-x-[17px] flex justify-center  gap-y-[17px] w-full">
-              <img
-                alt="Experience ratings left image"
-                src="https://c.animaapp.com/mhktpn46U9Eif8/assets/desktop-left.png"
-                className="text-transparent aspect-[auto_71_/_50] box-border hidden h-[50px] min-h-0 min-w-0 w-[71px] md:block md:min-h-[auto] md:min-w-[auto]"
-              />
-              <div className="text-white text-lg font-bold box-border caret-transparent tracking-[-0.18px] leading-[28.8px] text-center">
-                Over 4 million ⭐⭐⭐⭐⭐ experiences hosted by 15,000 hosts and
-                counting!
-              </div>
-              <div className="relative items-center box-border caret-transparent hidden min-h-0 min-w-0 md:flex md:min-h-[auto] md:min-w-[auto]">
-                <img
-                  alt="Experience ratings right image"
-                  src="https://c.animaapp.com/mhktpn46U9Eif8/assets/desktop-right.jpg"
-                  className="relative text-transparent aspect-[auto_41_/_41] box-border hidden h-[41px] mt-[-15px] min-h-0 min-w-0 w-[41px] rounded-[50%] md:block md:min-h-[auto] md:min-w-[auto]"
-                />
-                <div className="relative text-[22px] font-semibold items-center bg-fuchsia-200 box-border caret-transparent flex h-10 justify-center tracking-[-0.22px] leading-[22px] mb-[-15px] ml-[-13px] min-h-0 min-w-0 w-10 z-[1] rounded-[50%] md:min-h-[auto] md:min-w-[auto]">
-                  ❤️
-                </div>
-              </div>
-            </div>
-          </section>
         </div>
       </main>
     </div>
