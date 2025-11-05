2a4dd1d03049af48023ac4f5f84251c30ffe9950
shanelykins <shanewlykins@gmail.com>
2025-11-05 12:31:41 -0800

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   publicDir: "./static",
-  base: "./",
+  base: "/",
   css: {
     postcss: {
       plugins: [tailwind()],
