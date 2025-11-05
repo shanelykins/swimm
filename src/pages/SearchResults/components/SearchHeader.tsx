d9318c016745ad63188dbc6864a445ee62e1081a
shanelykins <shanewlykins@gmail.com>
2025-11-05 11:46:22 -0800

    Updated SearchHeader.tsx

--- src/pages/SearchResults/components/SearchHeader.tsx
+++ src/pages/SearchResults/components/SearchHeader.tsx
@@ -6,11 +6,7 @@ export const SearchHeader = () => {
     <header className="border-b border-gray-200 bg-white sticky top-0 z-50">
       <div className="flex items-center justify-between px-6 py-4">
         <Link to="/" className="flex items-center">
-          <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M24 44C35.046 44 44 35.046 44 24C44 12.954 35.046 4 24 4C12.954 4 4 12.954 4 24C4 35.046 12.954 44 24 44Z" fill="#0EA5E9"/>
-            <path d="M24 12C18.477 12 14 16.477 14 22C14 27.523 18.477 32 24 32C29.523 32 34 27.523 34 22C34 16.477 29.523 12 24 12Z" fill="white"/>
-            <path d="M20 26C20 28.209 21.791 30 24 30C26.209 30 28 28.209 28 26H20Z" fill="#0EA5E9"/>
-          </svg>
+          <img src="https://i.imgur.com/bEFjGvP.png" alt="Swimply" className="h-10" />
         </Link>
 
         <div className="flex items-center gap-4 flex-1 max-w-2xl mx-8">
